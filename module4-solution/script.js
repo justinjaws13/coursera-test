da715b5d77f05ba1404049c10c3aaf241d5fb9f9
Justin Quijada Hernandez <justin_quijada@hotmail.com>
2024-03-29 18:06:01 -0400

    changing

---
+++ module4-solution/script.js
@@ -0,0 +1,25 @@
+(function (window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // Define los objetos 'helloSpeaker' y 'byeSpeaker' y sus métodos 'speak'
+  var helloSpeaker = {
+    speak: function(name) {
+      console.log("Hello " + name);
+    }
+  };
+
+  var byeSpeaker = {
+    speak: function(name) {
+      console.log("Good Bye " + name);
+    }
+  };
+
+  for (var i in names) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})(window);
